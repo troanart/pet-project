33ef729f47d9bb515c8dc48006bf760a4a33eb15
Artem Troyan <troanart@gmail.com>
2025-11-20 23:07:28 +0200

    Обновление компонента статистики

--- src/app/home/components/hero-section/hero-section.component.ts
+++ src/app/home/components/hero-section/hero-section.component.ts
@@ -10,4 +10,23 @@ export class HeroSectionComponent {
   aboutButtonText: string = 'About us';
   coursesButtonRouterLink: string = '/courses';
   coursesButtonText: string = 'Explore courses';
+
+  statsList = [
+    {
+      title: 'Students graduated',
+      counter: 1200,
+    },
+    {
+      title: 'Completed courses',
+      counter: 84,
+    },
+    {
+      title: 'Qualified tutors',
+      counter: 16,
+    },
+    {
+      title: 'Years of experience',
+      counter: 5,
+    },
+  ];
 }
